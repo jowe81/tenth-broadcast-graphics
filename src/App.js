1412914e33158493c41da5ae5f1fcab4e05f0e40
Johannes Weber <johannes@drweber.de>
2022-06-14 15:55:43 -0700

    add <Canvas/>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss';
+import InputForm from './components/InputForm';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
+      <InputForm/>
+      <Canvas width="1920" height="1080"/>
     </div>
   );
 }
