682a3869f1949f58ab924e10ad82d9e059bd64ca
Johannes Weber <johannes@drweber.de>
2022-06-14 15:55:20 -0700

    add SubmitButton component

---
+++ src/components/InputForm/SubmitButton.js
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap';
+
+const SubmitButton = () => {
+  return (
+    <div className='submit-button'>
+      <Button>Process</Button>
+    </div>  
+  );
+};
+
+export default SubmitButton;
