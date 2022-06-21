a1fc7ca35a22005fbf8421872464a3b4ebc12a59
Johannes Weber <johannes@drweber.de>
2022-06-21 16:08:28 -0700

    modularize drawLowerThird, drawLowerThirdText

--- src/components/LowerThird.js
+++ src/components/LowerThird.js
@@ -1,20 +1,8 @@
 import Canvas from "./Canvas";
+import { drawLowerThird, drawLowerThirdText } from "../helpers/canvasFunctions";
 
-const LowerThird = props => {
-
-  //The semi-transparent lower third bar
-  const drawLowerThird = context => {
-    context.fillStyle = '#000000AA'
-    context.fillRect(0, 820, context.canvas.width, context.canvas.height)
-  }
-
-  const drawText = context => {
-    context.fillStyle = '#FFFFFF';
-    context.font = "30px Locator";
-    context.fillText('³ '+ props.text || '', 50, 950);    
-  };
-  
-  return (<Canvas {...props} drawBg={drawLowerThird} drawFg={drawText}/>);
+const LowerThird = props => {  
+  return (<Canvas {...props} drawBg={drawLowerThird} drawFg={drawLowerThirdText}/>);
 };
 
 export default LowerThird;
