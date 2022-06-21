76f485589f84e5a157faa991e9850d963514cf15
Johannes Weber <johannes@drweber.de>
2022-06-21 16:07:34 -0700

    add preview div

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,31 +1,37 @@
 
 import React, { useContext, useEffect } from 'react'
 import AppContext from '../context/AppContext';
+import './canvas.scss';
 
 const Canvas = props => {
   
   const appData = useContext(AppContext);
+  
   const canvasRef = appData.canvasRef;  
   
-  const { drawBg, width, height, drawFg } = props;
+  const { drawBg, width, height, drawFg, text } = props;
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
     const clearCanvas = context => context.clearRect(0, 0, 1920, 1080);
 
-    const locator = new FontFace('Locator', 'url(../vendor/Locator-Regular.otf)');
+    const font = new FontFace('Locator', 'url(../vendor/Locator-Regular.otf)');
 
-    locator.load().then(font => {    
+    font.load().then(font => {    
       document.fonts.add(font);
       clearCanvas(context);
       drawBg(context);
-      drawFg(context);      
+      drawFg(context, text);      
     });
 
-  }, [drawBg, drawFg, canvasRef]);
+  }, [drawBg, drawFg, canvasRef, text]);
   
-  return <canvas ref={canvasRef} width={width} height={height}/>
+  return (
+    <div id="preview">
+      <canvas ref={canvasRef} width={width} height={height}/>
+    </div>
+  );
 }
 
 export default Canvas;
